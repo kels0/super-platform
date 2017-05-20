f7e767259f575512dab5b5978ee1a952eaf7d07c
David Falk <davfa652@student.liu.se>
2017-05-20 12:07:45 +0200

    added bootstrap, JQuery and a menu

--- app/top-bar/top-bar.component.ts
+++ app/top-bar/top-bar.component.ts
@@ -1,18 +1,30 @@
 import {Component, NgZone} from '@angular/core';
 
+interface IMenuNames {
+    name: string;
+    value: string;
+}
+
 @Component({
     selector: 'top-bar',
+    //template: '<div *ngFor="let button in buttons" ><a href="#">{{button.name}}</a></li>',
     templateUrl: './app/top-bar/top-bar.component.html',
-    styleUrls: ["./app/top-bar/top-bar.component.less"]
+    styleUrls: ["./app/top-bar/top-bar.component.less"],
 })
-export class TopbarComponent {
 
+export class TopbarComponent {
     private title: string;
+    public buttons: IMenuNames[];
 
     constructor(
         private zone: NgZone
     ) {
         this.title = 'my awesome app';
+        this.buttons = [
+            { name: "Hem", value: "hem" },
+            { name: "Hitta Salong", value: "hitta" },
+            { name: "Boka", value: "boka" },
+            { name: "Logga in", value: "login" }
+        ];
     }
-
 }
